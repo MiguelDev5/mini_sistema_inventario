7cea070046951d0f0c7968e72cea99db6db4c8a3
MiguelDev5 <migangmedina@gmai.com>
2023-02-23 09:23:47 -0500

    Messages that are displayd when performing some operations in the program.

---
+++ utils/messages.py
@@ -0,0 +1,10 @@
+from utils.constants import *
+
+def print_warning(text):
+    print(ENTER, TAB, WARNING, text, WARNING, ENTER)
+
+def print_success(text):
+    print(ENTER, TAB, SUCCESS, text, SUCCESS, ENTER)
+
+def print_notify(text):
+    print(ENTER, TAB, text, ENTER)
