074cb207169ca9c6a3bec63f8b790c506731c07e
MiguelDev5 <migangmedina@gmai.com>
2023-02-23 09:22:24 -0500

    Constants of the entire program

---
+++ utils/constants.py
@@ -0,0 +1,55 @@
+# Estados en booleano
+RUN = True
+STOP = False
+
+# Estados en números
+ZERO = 0
+ONE = 1
+TEN = 10
+
+# Opciones generales
+NO_OPTION = '0'
+GENERAL_OPTION = '1'
+MAIN_MENU_OPTION = '2'
+EXIT = "SALIR"
+
+# Opciones del menú principal
+ADD = '1'
+DELETE = '2'
+UPDATE = '3'
+LOGOUT = '4'
+
+# Identificación de opciones por nombre
+ADD_OPTION = "Agregar"
+DELETE_OPTION = "Eliminar"
+UPDATE_OPTION = "Actualizar"
+
+# Header del menú
+TOP_MENU = """
+    ========================================
+    Lista de Productos:
+    ========================================
+    """
+
+# Footer del menú
+BOTTOM_MENU = """
+    ========================================
+    [1] Agregar, [2] Eliminar, [3] Actualizar, [4] Salir
+    """
+
+# Líne de separación del menú
+LINE = """
+    ========================================
+    """
+
+# Input aceptado de confirmación
+ACCEPT = ["S", "s", "si", "Si", "SI", "Sí", "sí", "SÍ"]
+
+# Espacios y saltos
+ENTER = "\n"
+TAB = "\t"
+
+#Mensajes predeterminados
+WARNING = "|---WARNING---|"
+SUCCESS = "|+++SUCCESS+++|"
+STRING10 = "          "
